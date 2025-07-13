b85b6943389316167c27c3f289be1cb7e5de6001
Eric Gorelik <egorelik93@users.noreply.github.com>
2025-07-13 19:52:26 -0400

    Restore uart reporting function calls in host.c

--- tmk_core/protocol/host.c
+++ tmk_core/protocol/host.c
@@ -133,8 +133,15 @@ led_t host_keyboard_led_state(void) {
     return (led_t)host_keyboard_leds();
 }
 
+extern void uart_send_report_repeat(void);
+extern void uart_send_mouse_report(void);
+extern void uart_send_consumer_report(void);
+extern void uart_send_system_report(void);
+
 /* send report */
 void host_keyboard_send(report_keyboard_t *report) {
+    uart_send_report_repeat();
+
     host_driver_t *driver = host_get_active_driver();
     if (!driver || !driver->send_keyboard) return;
 
@@ -169,6 +176,8 @@ void host_nkro_send(report_nkro_t *report) {
 }
 
 void host_mouse_send(report_mouse_t *report) {
+    uart_send_mouse_report();
+
     host_driver_t *driver = host_get_active_driver();
     if (!driver || !driver->send_mouse) return;
 
@@ -187,6 +196,8 @@ void host_system_send(uint16_t usage) {
     if (usage == last_system_usage) return;
     last_system_usage = usage;
 
+    uart_send_system_report();
+
     host_driver_t *driver = host_get_active_driver();
     if (!driver || !driver->send_extra) return;
 
@@ -200,6 +211,8 @@ void host_system_send(uint16_t usage) {
 void host_consumer_send(uint16_t usage) {
     if (usage == last_consumer_usage) return;
     last_consumer_usage = usage;
+
+    uart_send_consumer_report();
 
     host_driver_t *driver = host_get_active_driver();
     if (!driver || !driver->send_extra) return;
