57231f76cbb352151db4e9a2f032d5f1a01e3ec0
Eric Gorelik <egorelik93@users.noreply.github.com>
2025-07-15 17:59:09 -0400

    Added shortcuts for evil-mode commands

--- keyboards/nuphy/air75_v2/ansi/keymaps/egorelik93/keymap.c
+++ keyboards/nuphy/air75_v2/ansi/keymaps/egorelik93/keymap.c
@@ -17,16 +17,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+#define O_2_VI LT(2, KC_O)
+#define EVIL_U2D LT(2, KC_U)
+#define EVIL_Y LT(2, KC_Y)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-// layer Mac
+// layer gaming - activated by mac switch, since I don't use mac
 [0] = LAYOUT_75_ansi(
-    KC_ESC,     KC_BRID,   KC_BRIU,     KC_MCTL,   MAC_SEARCH, MAC_VOICE,   MAC_DND,   KC_MPRV,    KC_MPLY,     KC_MNXT,   KC_MUTE,    KC_VOLD,    KC_VOLU,     MAC_PRTA,   KC_INS,     KC_DEL,
-    KC_GRV,     KC_1,      KC_2,        KC_3,      KC_4,       KC_5,        KC_6,      KC_7,       KC_8,        KC_9,      KC_0,       KC_MINS,    KC_EQL,                  KC_BSPC,    KC_PGUP,
-    KC_TAB,     KC_Q,      KC_W,        KC_E,      KC_R,       KC_T,        KC_Y,      KC_U,       KC_I,        KC_O,      KC_P,       KC_LBRC,    KC_RBRC,                 KC_BSLS,    KC_PGDN,
-    KC_CAPS,    KC_A,      KC_S,        KC_D,      KC_F,       KC_G,        KC_H,      KC_J,       KC_K,        KC_L,      KC_SCLN,    KC_QUOT,                             KC_ENT,     KC_HOME,
+    KC_ESC,     KC_F1,     KC_F2,       KC_F3,     KC_F4,      KC_F5,       KC_F6,     KC_F7,      KC_F8,       KC_F9,     KC_F10,     KC_F11,     KC_F12,      KC_MUTE,    KC_INS,     KC_DEL,
+    KC_GRV,     KC_1,      KC_2,        KC_3,      KC_4,       KC_5,        KC_6,      KC_7,       KC_8,        KC_9,      KC_0,       KC_MINS,    KC_EQL,                  KC_BSPC,    KC_HOME,
+    KC_TAB,     KC_Q,      KC_W,        KC_E,      KC_R,       KC_T,        KC_Y,      KC_U,       KC_I,        KC_O,      KC_P,       KC_LBRC,    KC_RBRC,                 KC_BSLS,    KC_PGUP,
+  CTL_T(KC_F13),KC_A,      KC_S,        KC_D,      KC_F,       KC_G,        KC_H,      KC_J,       KC_K,        KC_L,      KC_SCLN,    KC_QUOT,                             KC_ENT,     KC_PGDN,
     KC_LSFT,               KC_Z,        KC_X,      KC_C,       KC_V,        KC_B,      KC_N,       KC_M,        KC_COMM,   KC_DOT,     KC_SLSH,                 KC_RSFT,    KC_UP,      KC_END,
-    KC_LCTL,    KC_LALT,   KC_LGUI,                                         KC_SPC,                             KC_RGUI,   MO(1),      KC_RCTL,                 KC_LEFT,    KC_DOWN,    KC_RGHT),
+    KC_LGUI,    MO(5),      KC_LALT,                                         KC_SPC,                             KC_RALT,   MO(3),      KC_RCTL,                 KC_LEFT,    KC_DOWN,    KC_RGHT),
 
 // layer Mac Fn
 [1] = LAYOUT_75_ansi(
@@ -41,10 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [2] = LAYOUT_75_ansi(
     KC_ESC,     KC_F1,     KC_F2,       KC_F3,     KC_F4,      KC_F5,       KC_F6,     KC_F7,      KC_F8,       KC_F9,     KC_F10,     KC_F11,     KC_F12,      KC_MUTE,    KC_INS,     KC_DEL,
     KC_GRV,     KC_1,      KC_2,        KC_3,      KC_4,       KC_5,        KC_6,      KC_7,       KC_8,        KC_9,      KC_0,       KC_MINS,    KC_EQL,                  KC_BSPC,    KC_HOME,
-    KC_TAB,     KC_Q,      KC_W,        KC_E,      KC_R,       KC_T,        KC_Y,      KC_U,       KC_I,        KC_O,      KC_P,       KC_LBRC,    KC_RBRC,                 KC_BSLS,    KC_PGUP,
+    KC_TAB,     KC_Q,      KC_W,        KC_E,      KC_R,       KC_T,        EVIL_Y,    EVIL_U2D,   KC_I,        O_2_VI,   KC_P,       KC_LBRC,    KC_RBRC,                 KC_BSLS,    KC_PGUP,
   CTL_T(KC_F13),KC_A,      KC_S,        KC_D,      KC_F,       KC_G,        KC_H,      KC_J,       KC_K,        KC_L,      KC_SCLN,    KC_QUOT,                             KC_ENT,     KC_PGDN,
     KC_LSFT,               KC_Z,        KC_X,      KC_C,       KC_V,        KC_B,      KC_N,       KC_M,        KC_COMM,   KC_DOT,     KC_SLSH,                 KC_RSFT,    KC_UP,      KC_END,
-    KC_LGUI,    MO(5),     KC_LALT,                                         KC_SPC,                             KC_RALT,   MO(3),      KC_RCTL,                 KC_LEFT,    KC_DOWN,    KC_RGHT),
+    KC_LGUI,    MO(5),      KC_LALT,                                         KC_SPC,                             KC_RALT,   MO(3),      KC_RCTL,                 KC_LEFT,    KC_DOWN,    KC_RGHT),
 
 // layer win Fn
 [3] = LAYOUT_75_ansi(
@@ -73,3 +77,80 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,                _______,    KC_CUT,    KC_COPY,    KC_PASTE,    _______,   _______,    _______,     _______,   _______,    _______,                 _______,    KC_PGUP,    _______,
     _______,    _______,    _______,                                        _______,                            _______,   _______,    _______,                 MS_WHLL,    KC_PGDN,    MS_WHLR)
 };
+
+static bool o_prefix_active = false;
+static bool u_prefix_active = false;
+static bool y_prefix_active = false;
+static bool vi_command_sent = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    bool any_vi_prefix_active = o_prefix_active || u_prefix_active || y_prefix_active;
+
+    switch (keycode) {
+        case O_2_VI:
+            if (record->event.pressed && !any_vi_prefix_active) {
+                // While examples also check record->tap.interrupted,
+                // for letter keys I do not want to accidentally hold
+                if (!record->tap.count || record->tap.interrupted) {
+                    o_prefix_active = true;
+                    vi_command_sent = false;
+                    return false;
+                }
+            } else {
+                o_prefix_active = false;
+                vi_command_sent = false;
+            }
+        case EVIL_U2D:
+            if (record->event.pressed && !any_vi_prefix_active) {
+                if (!record->tap.count || record->tap.interrupted) {
+                    u_prefix_active = true;
+                    vi_command_sent = false;
+                    return false;
+                }
+            } else {
+                u_prefix_active = false;
+                vi_command_sent = false;
+            }
+        case EVIL_Y:
+            if (record->event.pressed && !any_vi_prefix_active) {
+                if (!record->tap.count || record->tap.interrupted) {
+                    y_prefix_active = true;
+                    vi_command_sent = false;
+                    return false;
+                }
+            } else {
+                y_prefix_active = false;
+                vi_command_sent = false;
+            }
+        default:
+            if (any_vi_prefix_active
+                && record->event.pressed
+                && !vi_command_sent) {
+
+                tap_code(KC_F14);
+
+                if (u_prefix_active) {
+                    tap_code(KC_D);
+                }
+                if (y_prefix_active) {
+                    tap_code(KC_Y);
+                }
+
+                vi_command_sent = true;
+            }
+    }
+    return true;
+}
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case CTL_T(KC_F13):
+            return 100;
+        case O_2_VI:
+        case EVIL_Y:
+        case EVIL_U2D:
+            return 500;
+        default:
+            return TAPPING_TERM;
+    }
+}
