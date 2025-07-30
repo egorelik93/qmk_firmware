450a33d26cca9d718fa44fc2f8267f3532153f33
Eric Gorelik <egorelik93@users.noreply.github.com>
2025-07-30 16:00:29 -0400

    Added some tap dance functionality to keymap

--- keyboards/nuphy/air75_v2/ansi/keymaps/egorelik93/config.h
+++ keyboards/nuphy/air75_v2/ansi/keymaps/egorelik93/config.h
@@ -17,6 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define TAPPING_TERM 200
+
 #define TAPPING_TERM_PER_KEY
 #define FLOW_TAP_TERM 300
 
--- keyboards/nuphy/air75_v2/ansi/keymaps/egorelik93/keymap.c
+++ keyboards/nuphy/air75_v2/ansi/keymaps/egorelik93/keymap.c
@@ -25,6 +25,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EVIL_U2D LT(2, KC_U)
 #define EVIL_Y LT(2, KC_Y)
 
+// Tap Dance declarations
+enum {
+    TD_LGUI,
+    TD_LCTL,
+};
+
 // clang-format off
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,9 +58,9 @@ TOG_CAPS_IND,    SLEEP_TIMEOUT_DEC,    SLEEP_TIMEOUT_SHOW,    SLEEP_TIMEOUT_INC,
     KC_ESC,     KC_F1,     KC_F2,       KC_F3,     KC_F4,      KC_F5,       KC_F6,     KC_F7,      KC_F8,       KC_F9,     KC_F10,     KC_F11,     KC_F12,      KC_MUTE,    KC_INS,     KC_DEL,
     KC_GRV,     KC_1,      KC_2,        KC_3,      KC_4,       KC_5,        KC_6,      KC_7,       KC_8,        KC_9,      KC_0,       KC_MINS,    KC_EQL,                  KC_BSPC,    KC_HOME,
     KC_TAB,     KC_Q,      KC_W,        KC_E,      KC_R,       KC_T,        EVIL_Y,    EVIL_U2D,   KC_I,        O_2_VI,   KC_P,       KC_LBRC,    KC_RBRC,                 KC_BSLS,    KC_PGUP,
-  CTL_T(KC_F13),KC_A,      KC_S,        KC_D,      KC_F,       KC_G,        KC_H,      KC_J,       KC_K,        KC_L,      KC_SCLN,    KC_QUOT,                             KC_ENT,     KC_PGDN,
+    TD(TD_LCTL),KC_A,      KC_S,        KC_D,      KC_F,       KC_G,        KC_H,      KC_J,       KC_K,        KC_L,      KC_SCLN,    KC_QUOT,                             KC_ENT,     KC_PGDN,
     KC_LSFT,               KC_Z,        KC_X,      KC_C,       KC_V,        KC_B,      KC_N,       KC_M,        KC_COMM,   KC_DOT,     KC_SLSH,                 KC_RSFT,    KC_UP,      KC_END,
-    KC_LGUI,    MO(5),      KC_LALT,                                         KC_SPC,                             KC_RALT,   MO(3),      KC_RCTL,                 KC_LEFT,    KC_DOWN,    KC_RGHT),
+    TD(TD_LGUI),MO(5),      KC_LALT,                                         KC_SPC,                             KC_RALT,   MO(3),      KC_RCTL,                 KC_LEFT,    KC_DOWN,    KC_RGHT),
 
 // layer win Fn
 [3] = LAYOUT_75_ansi(
@@ -82,6 +88,79 @@ TOG_CAPS_IND,    SLEEP_TIMEOUT_DEC,    SLEEP_TIMEOUT_SHOW,    SLEEP_TIMEOUT_INC,
     _______,    _______,    _______,    _______,   _______,    _______,     _______,   _______,    _______,     _______,   _______,    _______,                             _______,    _______,
     _______,                _______,    KC_CUT,    KC_COPY,    KC_PASTE,    _______,   _______,    _______,     _______,   _______,    _______,                 _______,    KC_PGUP,    _______,
     _______,    _______,    _______,                                        _______,                            _______,   _______,    _______,                 MS_WHLL,    KC_PGDN,    MS_WHLR)
+};
+
+typedef struct {
+    uint16_t tap;
+    uint16_t hold;
+    void (*double_tap_fn)(void);
+    uint16_t held;
+} tap_dance_double_tap_hold_t;
+
+void tap_dance_double_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
+    tap_dance_double_tap_hold_t *tap_hold = (tap_dance_double_tap_hold_t *)user_data;
+
+    if (state->pressed
+/*#ifndef PERMISSIVE_HOLD
+        && !state->interrupted
+#else*/
+    ) {
+        if (state->count >= 2) {
+            tap_code16(tap_hold->tap);
+        }
+
+        register_code16(tap_hold->hold);
+        tap_hold->held = 2;
+    } else {
+        if (state->count == 2) {
+            tap_hold->held = 3;
+        } else {
+            register_code16(tap_hold->tap);
+            tap_hold->held = 1;
+        }
+    }
+}
+
+void tap_dance_double_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
+    tap_dance_double_tap_hold_t *tap_hold = (tap_dance_double_tap_hold_t *)user_data;
+
+    switch (tap_hold->held) {
+        case 1:
+            unregister_code16(tap_hold->tap);
+            tap_hold->held = 0;
+            break;
+        case 2:
+            unregister_code16(tap_hold->hold);
+            tap_hold->held = 0;
+            break;
+        case 3:
+            (*tap_hold->double_tap_fn)();
+            tap_hold->held = 0;
+            break;
+    }
+}
+
+#define ACTION_TAP_DANCE_DOUBLE_TAP_HOLD(tap, hold, double_tap)                                        \
+    {                                                                               \
+        .fn        = {NULL, tap_dance_double_tap_hold_finished, tap_dance_double_tap_hold_reset, NULL}, \
+        .user_data = (void *)&((tap_dance_double_tap_hold_t){tap, hold, double_tap, 0}),               \
+    }
+
+
+void lgui_command_palatte(void) {
+    register_mods(MOD_LGUI | MOD_LALT);
+    tap_code(KC_SPC);
+    unregister_mods(MOD_LGUI | MOD_LALT);
+}
+
+void lctl_double_tap(void) {
+    tap_code(KC_F15);
+}
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_LGUI] = ACTION_TAP_DANCE_DOUBLE_TAP_HOLD(KC_LGUI, KC_LGUI, &lgui_command_palatte),
+    [TD_LCTL] = ACTION_TAP_DANCE_DOUBLE_TAP_HOLD(KC_F13, KC_LCTL, &lctl_double_tap),
 };
 
 static bool o_prefix_active = false;
@@ -122,6 +201,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
                 y_prefix_active = false;
             }
+        // This adds extra keys in my current usage, but leaving commented as an example.
+        /*case TD(TD_LGUI): // list all tap dance keycodes with tap-hold configurations
+            tap_dance_action_t *action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
+            if (!record->event.pressed && action->state.count && !action->state.finished) {
+                tap_dance_double_tap_hold_t *tap_hold = (tap_dance_double_tap_hold_t *)action->user_data;
+                if (action->state.count == 2) {
+                    (*tap_hold->double_tap_fn)();
+                }
+            }*/
         default:
             if (any_vi_prefix_active
                 && record->event.pressed
@@ -143,6 +231,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case CTL_T(KC_F13):
+        case TD(TD_LCTL):
             return 200;
         case O_2_VI:
         case EVIL_Y:
@@ -150,5 +239,16 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
             return 400;
         default:
             return TAPPING_TERM;
+    }
+}
+
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case TD(TD_LCTL):
+            // Immediately select the hold action when another key is tapped.
+            return true;
+        default:
+            // Do not select the hold action when another key is tapped.
+            return false;
     }
 }
