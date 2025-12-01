40bbdf80f5a1f91ead3ebf8d52681206a33b0170
Eric Gorelik <egorelik93@users.noreply.github.com>
2025-11-30 21:40:56 -0500

    Revert evil letter shifts

--- keyboards/nuphy/air75_v2/ansi/keymaps/egorelik93/keymap.c
+++ keyboards/nuphy/air75_v2/ansi/keymaps/egorelik93/keymap.c
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdint.h>
+#include <stdio.h>
 #include "quantum.h"
 #include "ansi.h"
 #include "keycodes.h"
@@ -27,8 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DSLEEP_TIMEOUT_DEC DEEP_SLEEP_TIMEOUT_DEC
 #define DSLEEP_TIMEOUT_SHOW DEEP_SLEEP_TIMEOUT_SHOW
 
-#define P_2_VI LT(2, KC_P)
-#define EVIL_M2D LT(2, KC_M)
+#define O_2_VI LT(2, KC_O)
+#define EVIL_U2D LT(2, KC_U)
 #define EVIL_Y LT(2, KC_Y)
 
 // Tap Dance declarations
@@ -65,9 +66,9 @@ TOG_CAPS_IND,    SLEEP_TIMEOUT_DEC,    SLEEP_TIMEOUT_SHOW,    SLEEP_TIMEOUT_INC,
 [2] = LAYOUT_75_ansi(
     KC_ESC,     KC_F1,     KC_F2,       KC_F3,     KC_F4,      KC_F5,       KC_F6,     KC_F7,      KC_F8,       KC_F9,     KC_F10,     KC_F11,     KC_F12,      KC_MUTE,    KC_INS,     KC_DEL,
     KC_GRV,     KC_1,      KC_2,        KC_3,      KC_4,       KC_5,        KC_6,      KC_7,       KC_8,        KC_9,      KC_0,       KC_MINS,    KC_EQL,                  KC_BSPC,    KC_HOME,
-    KC_TAB,     KC_Q,      KC_W,        KC_E,      KC_R,       KC_T,        EVIL_Y,    KC_U,       KC_I,        KC_O,      P_2_VI,     KC_LBRC,    KC_RBRC,                 KC_BSLS,    KC_PGUP,
+    KC_TAB,     KC_Q,      KC_W,        KC_E,      KC_R,       KC_T,        EVIL_Y,    EVIL_U2D,   KC_I,        O_2_VI,    KC_P,       KC_LBRC,    KC_RBRC,                 KC_BSLS,    KC_PGUP,
     TD(TD_LCTL),KC_A,      KC_S,        KC_D,      KC_F,       KC_G,        KC_H,      KC_J,       KC_K,        KC_L,      KC_SCLN,    KC_QUOT,                             KC_ENT,     KC_PGDN,
-    KC_LSFT,               KC_Z,        KC_X,      KC_C,       KC_V,        KC_B,      KC_N,   EVIL_M2D,        KC_COMM,   KC_DOT,     KC_SLSH,                 KC_RSFT,    KC_UP,      KC_END,
+    KC_LSFT,               KC_Z,        KC_X,      KC_C,       KC_V,        KC_B,      KC_N,       KC_M,        KC_COMM,   KC_DOT,     KC_SLSH,                 KC_RSFT,    KC_UP,      KC_END,
     TD(TD_LGUI),MO(5),     TD(TD_LALT),                                     KC_SPC,                         TD(TD_RALT),   MO(3),      KC_RCTL,                 KC_LEFT,    KC_DOWN,    KC_RGHT),
 
 // layer win Fn
@@ -183,37 +184,36 @@ tap_dance_action_t tap_dance_actions[] = {
 
 #define EVIL_TAPPING_TERM 150
 
-static bool p_prefix_active = false;
-static bool m_prefix_active = false;
+static bool o_prefix_active = false;
+static bool u_prefix_active = false;
 static bool y_prefix_active = false;
 static bool vi_command_incomplete = false;
 static bool vi_command_sent = false;
-static uint16_t p_timer = 0;
-static uint16_t m_timer = 0;
+static uint16_t o_timer = 0;
+static uint16_t u_timer = 0;
 static uint16_t y_timer = 0;
 static uint16_t vi_mod_handling = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    bool any_vi_prefix_active = p_prefix_active || m_prefix_active || y_prefix_active;
+    bool any_vi_prefix_active = o_prefix_active || u_prefix_active || y_prefix_active;
 
     switch (keycode) {
-        case P_2_VI:
+        case O_2_VI:
             if (record->event.pressed && !any_vi_prefix_active) {
                 // While examples also check record->tap.interrupted,
                 // for letter keys I do not want to accidentally hold
                 if (!record->tap.count) {
-                    p_prefix_active = true;
-                    p_timer = timer_read();
+                    o_prefix_active = true;
+                    o_timer = timer_read();
                     return false;
                 }
             } else {
-                bool no_process = p_prefix_active;
-                if (p_prefix_active && !vi_command_sent) {
-                    tap_code(KC_P);
+                bool no_process = o_prefix_active;
+                if (o_prefix_active && !vi_command_sent) {
+                    tap_code(KC_O);
                 }
-
-                p_timer = 0;
-                p_prefix_active = false;
+                o_timer = 0;
+                o_prefix_active = false;
 
                 if (no_process) {
                     vi_command_sent = false;
@@ -221,21 +221,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     return false;
                 }
             }
-        case EVIL_M2D:
+        case EVIL_U2D:
             if (record->event.pressed && !any_vi_prefix_active) {
-                if (!record->tap.count) {
-                    m_prefix_active = true;
-                    m_timer = timer_read();
+                if (!record->tap.count && !get_mods()) {
+                    u_prefix_active = true;
+                    u_timer = timer_read();
                     return false;
                 }
             } else {
-                bool no_process = m_prefix_active;
-                if (m_prefix_active && !vi_command_sent) {
-                    tap_code(KC_M);
+                bool no_process = u_prefix_active;
+                if (u_prefix_active && !vi_command_sent) {
+                    tap_code(KC_U);
                 }
 
-                m_timer = 0;
-                m_prefix_active = false;
+                u_timer = 0;
+                u_prefix_active = false;
 
                 if (no_process) {
                     vi_command_sent = false;
@@ -245,7 +245,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
         case EVIL_Y:
             if (record->event.pressed && !any_vi_prefix_active) {
-                if (!record->tap.count) {
+                if (!record->tap.count && !get_mods()) {
                     y_prefix_active = true;
                     y_timer = timer_read();
                     return false;
@@ -306,17 +306,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 && record->event.pressed
                 && !vi_command_incomplete) {
 
-                uint16_t p_elapsed = timer_elapsed(p_timer);
-                uint16_t m_elapsed = timer_elapsed(m_timer);
+                uint16_t o_elapsed = timer_elapsed(o_timer);
+                uint16_t u_elapsed = timer_elapsed(u_timer);
                 uint16_t y_elapsed = timer_elapsed(y_timer);
 
-                if ((p_prefix_active && p_elapsed >= EVIL_TAPPING_TERM) ||
-                    (m_prefix_active && m_elapsed >= EVIL_TAPPING_TERM) ||
+                if ((o_prefix_active && o_elapsed >= EVIL_TAPPING_TERM) ||
+                    (u_prefix_active && u_elapsed >= EVIL_TAPPING_TERM) ||
                     (y_prefix_active && y_elapsed >= EVIL_TAPPING_TERM)) {
 
                     tap_code(KC_F14);
 
-                    if (m_prefix_active && timer_elapsed(m_timer) >= EVIL_TAPPING_TERM) {
+                    if (u_prefix_active && timer_elapsed(u_timer) >= EVIL_TAPPING_TERM) {
                         tap_code(KC_D);
                     }
                     if (y_prefix_active && timer_elapsed(y_timer) >= EVIL_TAPPING_TERM) {
@@ -342,14 +342,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
                         return false;
                     }
-                } else if (p_prefix_active && p_elapsed < EVIL_TAPPING_TERM) {
-                    tap_code(KC_P);
-                    p_timer = 0;
-                    p_prefix_active = false;
-                } else if (m_prefix_active && m_elapsed < EVIL_TAPPING_TERM) {
-                    tap_code(KC_M);
-                    m_timer = 0;
-                    m_prefix_active = false;
+                } else if (o_prefix_active && o_elapsed < EVIL_TAPPING_TERM) {
+                    tap_code(KC_O);
+                    o_timer = 0;
+                    o_prefix_active = false;
+                } else if (u_prefix_active && u_elapsed < EVIL_TAPPING_TERM) {
+                    tap_code(KC_U);
+                    u_timer = 0;
+                    u_prefix_active = false;
                 } else if (y_prefix_active && y_elapsed < EVIL_TAPPING_TERM) {
                     tap_code(KC_Y);
                     y_timer = 0;
